43cdd3a9c3567ac50ad4a70cd1860d9d04137a3e
bartholomewdevelopment <joseph@bartholomewdevelopment.com>
2025-03-10 23:33:51 -0400

    exercise 4

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,69 @@
 using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        // Lesson Notes
+        // List<int> numbers = new List<int>();
+        // List<string> words = new List<string>();
+
+        // words.Add("phone");
+        // words.Add("keyboard");
+        // words.Add("Mouse");
+        // numbers.Add(16);
+        // numbers.Add(17);
+        // numbers.Add(18);
+
+        // Console.WriteLine(words.Count);
+        // Console.WriteLine(numbers.Sum());
+
+        // foreach (string word in words)
+        // {
+        //     Console.WriteLine(word);
+        // }
+
+        // for (int i = 0; i < words.Count; i++)
+        // {
+        //     Console.WriteLine(words[i]);
+        // }
+
+        List<int> numbers = new List<int>();
+        int userNum = -1;
+        while (userNum != 0)
+        {
+            Console.Write("Enter a number, when you are done, type 0. ");
+            string userResponse = Console.ReadLine();
+            userNum = int.Parse(userResponse);
+
+            if (userNum != 0)
+            {
+                numbers.Add(userNum);
+            }
+        }
+        
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+
+        Console.WriteLine($"The sum is: {sum}");
+
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+        int max = numbers[0];
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+        Console.WriteLine($"The max is {max}");
     }
 }
