7cbaa172b19c084ab4f7427e19e440359e19cb35
bartholomewdevelopment <joseph@bartholomewdevelopment.com>
2025-03-10 22:55:07 -0400

    exercise 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        // Console.Write("What is your magic number? ");
+        // int magicnum = int.Parse(Console.ReadLine());
+
+        Random randomGenerator = new Random();
+        int magicnum = randomGenerator.Next(1, 11);
+
+        int guess = -1;
+
+        Console.WriteLine("I have randomly chosen a number between 1-10.  Try to guess it!");
+        
+        while (guess != magicnum)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+        
+            if (guess > magicnum)
+            {
+                Console.WriteLine("Your guess is too high");
+            }
+            else if (guess < magicnum)
+            {
+                Console.WriteLine("Your guess is too low");
+            }
+        }
+         
+        Console.Write("You guessed correctly!");
+            
+
     }
 }
